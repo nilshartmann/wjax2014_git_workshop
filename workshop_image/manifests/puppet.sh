7ff27cebab4d28c2fbaca922e95fa82eb678eb0c
Nils Hartmann <nils@nilshartmann.net>
2014-09-14 11:34:56 +0200

    Macbook-Keyboard-Layout (funktioniert noch nicht)

--- workshop_image/manifests/puppet.sh
+++ workshop_image/manifests/puppet.sh
@@ -1,6 +1,16 @@
 #!
 #keyboard in german
+
+echo KEYBOARD WIRD KOPIERT
+
 cp /vagrant/data/keyboard /etc/default/keyboard
+cp /vagrant/data/keyboard /etc/default/keyboard_german
+#keyboard macbook
+cp /vagrant/data/keyboard_macbook /etc/default/keyboard_macbook
+
+cp /vagrant/data/set-keyboard-macbook.sh /usr/local/bin/set-keyboard-macbook.sh
+chmod 775 /usr/local/bin/set-keyboard-macbook.sh
+
 #Load Keyboard changes
 udevadm trigger --subsystem-match=input --action=change
 
