fcd11e209f9cfdfc0fc3a8274573bb54f56464e6
Nils Hartmann <nils@nilshartmann.net>
2014-10-27 17:51:22 +0100

    Atlassian jgitflow Plug-in initialisieren

--- workshop_image/data/init-maven-and-gradle.sh
+++ workshop_image/data/init-maven-and-gradle.sh
@@ -24,8 +24,29 @@ mvn release:perform
 # Release wieder entfernen
 rm -rf /opt/workshop/mvn-repo/de
 rm -rf /opt/workshop/git-repos/web-application.git
+
+
+# Atlassian Maven Plug-in
+rm -rf /tmp/web-application
+cd /tmp
+cp -r /home/vagrant/repos/build/atlassian-jgitflow-plugin/web-application.git/ .
+git clone web-application.git
+cd web-application
+mvn jgitflow:feature-start --batch-mode -DfeatureName=init
+mvn jgitflow:feature-finish --batch-mode
+mvn jgitflow:release-start --batch-mode
+mvn jgitflow:release-finish --batch-mode
+
+# Aufraeumen
+rm -rf /tmp/web-application.git
+rm -rf /tmp/web-application
+rm -rf /opt/workshop/mvn-repo/de
+rm -rf /opt/workshop/git-repos/web-application.git
+
 cp -r /home/vagrant/repos/build/maven-rel-plugin/web-application.git/ /opt/workshop/git-repos
 
+
+# Repository Backup anlegen
 cd /home/vagrant
 tar czvf _repos-backup.tar.gz repos/
 
